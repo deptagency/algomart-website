341bfb61b141d8755a6884fbd68fb3b5fafe74c2
Brandon Aaskov <brandonaaskov@gmail.com>
2022-04-18 21:38:45 -0400

    makes the header sticky

--- src/pages/index.js
+++ src/pages/index.js
@@ -123,7 +123,7 @@ function classNames(...classes) {
 export default function HomePage() {
   return (
     <div className="bg-white">
-      <Disclosure as="nav" className="bg-gray-900">
+      <Disclosure as="nav" className="bg-gray-900 sticky top-0 z-50">
         {({ isMobileMenuOpen }) => (
           <>
             <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
